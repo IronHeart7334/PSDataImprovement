6f7b3ab89ed51b9edfaa59e6cd213f7c14c97961
IronHeart7334 <greengrappler12@gmail.com>
2019-10-02 12:58:28 -0700

    added JQuery. couldn't get it to add dynamically, so I just had to download and add it to the project

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,7 @@
 // do DOM based stuff with this
 // https://developer.chrome.com/extensions/content_scripts
-/*
-let jq1 = document.createElement("script");
-jq1.src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js";
+console.log($);
 
-let jq2 = document.createElement("script");
-jq2.src = "";
-document.body.appendChild();
-*/
 console.log("Loaded from content.js");
 console.log(document);
 
