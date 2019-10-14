4d1842f4f98693053c4725a62e97edb58e87df64
Matt Crow <greengrappler12@gmail.com>
2019-10-14 14:49:01 -0700

    Next time, I'll implement the PO history querying
    Once I don't have access to Randy's office, I'll work on cleaning everything up.

--- scripts/popup.js
+++ scripts/popup.js
@@ -37,6 +37,28 @@ $("#reqSubmit").click(()=>{
     reader.readAsText(file, "UTF-8");
 });
 
+$("#poFile").click(()=>{
+    let file = $("#poFile").get(0).files[0];
+    let reader = new FileReader();
+    reader.onload = async(e)=>{
+        let text = e.target.result;
+        let data = text.split(NEWLINE).map((line)=>line.split(","));
+        let reqIdCol = data[0].indexOf("Req ID");
+        let poIdCol = data[0].indexOf("PO ID");
+        let newText = "Req ID, PO ID";
+        data.shift(); //remove header
+        data.forEach((line)=>{
+            newText += NEWLINE + line[reqIdCol] + "," + line[poIdCol];
+        });
+        
+        await set("poFile", newText);
+        await set("poResult", "");
+        await set("autoclick", $("#autoclick").is(":checked"));
+        setUrl("https://psreports.losrios.edu/PO_History.asp");
+    };
+    reader.readAsText(file, "UTF-8");
+});
+
 function setUrl(newUrl){
     chrome.tabs.query({currentWindow: true, active: true}, (tab)=>{
         chrome.tabs.update(tab.id, {url: newUrl});
