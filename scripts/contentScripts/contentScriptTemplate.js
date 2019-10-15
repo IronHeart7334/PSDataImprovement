5627686abf5c78ad26df62e7d0ea3999acf4f82d
IronHeart7334 <greengrappler12@gmail.com>
2019-10-15 11:07:03 -0700

    Done with inputData method, next, readResult

--- scripts/contentScripts/contentScriptTemplate.js
+++ scripts/contentScripts/contentScriptTemplate.js
@@ -6,17 +6,17 @@
 
 class ContentScript{
     /*
-     * orderFileName: the key in local storage which links to
+     * queryFileName: the key in local storage which links to
      *  the data used by this script
      * inputURL: the pathname of the page where the script
      *  should imitate a user entering input.
      * resultURL: the pathname of the page where the script
      *  should extract data from the result of its query
      */
-    async constructor(orderFileName, inputURL, resultURL){
-        let data = await get(orderFileName);
+    async constructor(queryFileName, inputURL, resultURL){
+        let data = await get(queryFileName);
         if(data === null){
-            throw new Error(`Nothing is set in local storage for key "${orderFileName}"`);
+            throw new Error(`Nothing is set in local storage for key "${queryFileName}"`);
         }
         
         if(!inputURL.startsWith("/")){
@@ -26,9 +26,10 @@ class ContentScript{
             resultURL += "/";
         }
         
-        this.orderFileName = orderFileName;
+        this.queryFileName = queryFileName;
         this.inputURL = inputURL;
         this.resultURL = resultURL;
+        this.autoclick = await get("autoclick");
     }
     
     checkURL(){
@@ -44,8 +45,24 @@ class ContentScript{
         }
     }
     
-    inputData(){
+    async inputData(){
+        let remainingQueries = await get(this.queryFileName);
+        if(remainingQueries === null || remainingQueries.trim() === ""){
+            throw new Error("No queries remain, yet the script continues to run: it should be done by now.");
+        }
+        let qArray = remainingQueries.split(NEWLINE);
+        /*
+        Since this script is reloaded every time it changes pages,
+        we cannot simply iterate over each query to plug them in one at a time,
+        instead, we must delete queries from the query list whenever we input them.
+         */
+        let currQ = qArray.shift().split(",");
+        remainingQueries = qArray.join("\n"); //we just deleted the first row
+        console.log(`Inputting query [${currQ.join(", ")}]`);
+        console.log("Remaining queries are: \n" + remainingQueries);
         
+        await set(this.queryFileName, remainingQueries);
+        await processQuery(currQ);
     }
     
     readResult(){
@@ -54,12 +71,16 @@ class ContentScript{
     
     run(){
         $(window).on("load", async()=>{
-            let order = await get(this.orderFileName);
+            let order = await get(this.queryFileName);
             if(order === null){
                 console.log("Script is done. Do nothing.");
             } else {
                 checkURL();
             }
         });
+    }
+    
+    processQuery(query){
+        throw new Error("method processQuery(query) is abstract; thus, it must be overridden.");
     }
 }
