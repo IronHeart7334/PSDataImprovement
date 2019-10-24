2a165c9f41fd613368ae4ce27c28e1de982bdcbb
IronHeart7334 <greengrappler12@gmail.com>
2019-10-24 11:12:28 -0700

    Will need to see if the POInformation works, as when I manually queried the data we got, nothing came up. I'm not even sure Randy needs this one

---
+++ scripts/content/poInfoContent.js
@@ -0,0 +1,29 @@
+class PoInfoContentScript extends ContentScript{
+    constructor(){
+        super("poInfoFile", "poInfoResult", "PurchaseOrderInformation.asp", "PPurchaseOrderInformationQ.asp");
+    }
+    
+    async processQuery(query){
+        $('input[name="PurchaseOrderNumber"]').val(query[1]);
+        $('input[name="RequisitionNumber"]').val(query[0]);
+        let autoclick = await get("autoclick");
+        if(autoclick){
+            $('[name="B1"]')[0].click();
+        }
+    }
+    
+    async processResult(){
+        return $("table[border=1]").eq(1).table2CSV({
+            delivery: "value"
+        });
+    }
+    
+    // this part should click the back button
+    async postProcessResult(){
+        let autoclick = await get("autoclick");
+        if(autoclick){
+            $("a[href='PurchaseOrderInformation.asp']")[0].click();
+        }
+    }
+}
+new PoInfoContentScript().run();
--- scripts/popup.js
+++ scripts/popup.js
@@ -19,7 +19,6 @@ linkButton("acctSubmit", "acctFile", "acctResult", "https://psreports.losrios.ed
 linkButton("reqSubmit", "reqFile", "reqResult", "https://psreports.losrios.edu/REQ_History.asp");
 
 //needs special behaviour
-//working here
 $("#poSubmit").click(()=>{
     let file = $("#poFile").get(0).files[0];
     let reader = new FileReader();
@@ -53,6 +52,43 @@ $("#poSubmit").click(()=>{
         await set("poResult", "");
         await set("autoclick", $("#autoclick").is(":checked"));
         setUrl("https://psreports.losrios.edu/PO_History.asp");
+    };
+    reader.readAsText(file, "UTF-8");
+});
+
+$("#poInfoSubmit").click(()=>{
+    let file = $("#poInfoFile").get(0).files[0];
+    let reader = new FileReader();
+    reader.onload = async(e)=>{
+        let text = e.target.result;
+        let data = text.split(NEWLINE).map((line)=>line.split(","));
+        //the file has quote marks around just about everything
+        let reqIdCol = data[0].indexOf('"Req ID"');
+        let poIdCol = data[0].indexOf('"PO ID"');
+        let newText = "";
+        data.shift(); //remove header
+        let reqId;
+        let poId;
+        //convert to reqId, poId
+        data
+            .filter((line)=>line[poIdCol] !== "PO is awaiting dispatch" && line[poIdCol].trim() !== "")
+            .forEach((line)=>{
+                //need to remove quote marks around each entry
+                reqId = line[reqIdCol].trim();
+                poId = line[poIdCol].trim();
+                while(reqId.indexOf('"') !== -1){
+                    reqId = reqId.replace('"', '');
+                }
+                while(poId.indexOf('"') !== -1){
+                    poId = poId.replace('"', '');
+                }
+                newText += "\n" + reqId + "," + poId;
+            });
+        
+        await set("poInfoFile", newText);
+        await set("poInfoResult", "");
+        await set("autoclick", $("#autoclick").is(":checked"));
+        setUrl("https://psreports.losrios.edu/PurchaseOrderInformation.asp");
     };
     reader.readAsText(file, "UTF-8");
 });
